31cb1ed24ab42493b92d1ee71eec7488471fefef
l9jmpaz <l9jmpaz@gmail.com>
2025-11-03 01:20:59 +0800

    11

--- models/User.js
+++ models/User.js
@@ -5,6 +5,8 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   phone: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  barangay: {type: String, required: true },
+  profilePicture: {type: String, default: ""},
   role: { type: String, default: "user" },
   status: { type: String, default: "Active" },
 
