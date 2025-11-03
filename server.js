c195232833928729ffda0658d204b5b27407426a
l9jmpaz <l9jmpaz@gmail.com>
2025-11-03 10:58:50 +0800

    23

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ connectDB();
 const app = express();
 app.use(cors({origin: "*",methods:["GET", "POST", "PUT", "DELETE"],allowedHeaders: ["Content-Type","Authorization"]}));
 app.use(express.json());
-app.use(express.urlencoded({ exended: true}));
+app.use(express.urlencoded({ extended: true}));
 // ✅ Mount routes
 app.use("/api/activities", activityRoutes);
 app.use("/api/support", supportRoutes);
